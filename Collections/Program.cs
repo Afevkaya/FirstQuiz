427b664989aa171526a7ac3d5e385a0b5e09194a
Afevkaya <Afevkaya5151@gmail.com>
2022-08-13 13:52:30 +0300

    Collections ar over.

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -3,18 +3,22 @@
 using Collections;
 
 // List Kullanımı
+//Ekleme
 /*var customers = new List<Customer>();
 customers.Add(new Customer { Name = "Ahmet", Surname = "Evkaya"});
 customers.Add(new Customer { Name = "Kemal", Surname = "Şahin" });
 customers.Add(new Customer { Name = "Enes", Surname = "Çömez" });
 
 
+// Listeleme
 customers.ForEach(c =>
 {
     Console.WriteLine($"{c.Name} - {c.Surname}");
 });
 
 Console.WriteLine("-----------------------");
+
+// Silme
 var customer = customers.Find(c => c.Name == "Ahmet");
 if (customer is not null)
 {
@@ -31,6 +35,8 @@ customers.ForEach(c =>
 });
 
 Console.WriteLine("-----------------------");
+
+// Filtreleme
 var customer2 = customers.Find(c => c.Name == "Enes");
 if (customer2 is not null)
 {
@@ -39,11 +45,13 @@ if (customer2 is not null)
 
 
 //LinkedList
+//Ekleme
 /*var customers2 = new LinkedList<Customer>();
 customers2.AddFirst(new Customer {Name = "Ahmet", Surname = "Evkaya"});
 customers2.AddLast(new Customer {Name = "Kemal", Surname = "Şahin"});
 customers2.AddFirst(new Customer {Name = "Enes", Surname = "Çömez"});
 
+//Listeleme
 foreach (var c in customers2)
 {
     Console.WriteLine($"{c.Name} - {c.Surname}");
@@ -62,6 +70,7 @@ foreach (var c in customers2)
     }
 }
 
+//Silme
 if (customer2 is not null)
 {
     Console.WriteLine($"{customer2.Name} adlı kullanıcı silinmiştir");
@@ -74,6 +83,7 @@ else
 
 Console.WriteLine("-----------------------");
 
+//Filtreleme
 Customer customerr2 = null;
 foreach (var c in customers2)
 {
@@ -91,10 +101,51 @@ if (customerr2 is not null)
 }*/
 
 
-Dictionary<int , Customer> customers3 = new Dictionary<int , Customer>();
+//Dictionary
+//Ekleme
+/*Dictionary<int , Customer> customers3 = new Dictionary<int , Customer>();
 customers3.Add(1, new Customer() {Name = "Ahmet", Surname = "Evkaya"});
 customers3.Add(2, new Customer() {Name = "Kemal", Surname = "Şahin"});
 customers3.Add(3, new Customer() {Name = "Enes", Surname = "Çömez"});
 
+//Listeleme
+foreach (var customer in customers3)
+{
+    Console.WriteLine($"{customer.Key} - {customer.Value.Name} {customer.Value.Surname}");
+}
 
+Console.WriteLine("---------------------");
+// Silme
+int customerKey = 0;
+foreach (var customer in customers3)
+{
+    if (customer.Value.Name == "Betül")
+    {
+        customerKey = customer.Key;
+        break;
+    }
+}
+
+if (customerKey > 0)
+{
+    Console.WriteLine($"{customers3.FirstOrDefault(c=>c.Key == customerKey).Value.Name} adlı müşteri silinmiştir");
+    customers3.Remove(customerKey);
+}
+else
+{
+    Console.WriteLine("Müşteri bulunamamıştır");
+}
+
+Console.WriteLine("---------------------");
+//Filtreleme
+var customer3 = customers3.FirstOrDefault(c => c.Key == 2);
+if (customer3.Value is not null)
+{
+    Console.WriteLine($"{customer3.Value.Name} adlı müşteri bulunmuştur.");
+}
+else
+{
+    Console.WriteLine("Müşteri bulunamamıştır.");
+}
+*/
 
